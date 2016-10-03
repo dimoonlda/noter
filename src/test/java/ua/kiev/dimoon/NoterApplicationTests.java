32c633cb308a84cd7bdb20dd482fcb19ec32cba6
admin <dimoon.lda@gmail.com>
2016-10-04 00:37:57 +0300

    initial commit

---
+++ src/test/java/ua/kiev/dimoon/NoterApplicationTests.java
@@ -0,0 +1,16 @@
+package ua.kiev.dimoon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NoterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
