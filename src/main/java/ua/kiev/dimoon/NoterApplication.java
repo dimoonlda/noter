27e3cc29ef35aa69ec24e785650186341b5d0184
admin <dimoon.lda@gmail.com>
2016-10-04 00:37:57 +0300

    initial commit

---
+++ src/main/java/ua/kiev/dimoon/NoterApplication.java
@@ -0,0 +1,12 @@
+package ua.kiev.dimoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoterApplication.class, args);
+	}
+}
